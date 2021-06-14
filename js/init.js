15f074a98b156d6e61ebd058b16030cf68c8eb76
JocelynProyec50 <85643687+JocelynProyec50@users.noreply.github.com>
2021-06-14 00:34:56 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/* Inicializa Firebase con la configuración del proyecto.*/
+//@ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBmzmkHp6Fg8Ep_YvfjQ9pAGwCY5C0L30A",
+    authDomain: "jocelyn50proyec.firebaseapp.com",
+    projectId: "jocelyn50proyec",
+    storageBucket: "jocelyn50proyec.appspot.com",
+    messagingSenderId: "383184248793",
+    appId: "1:383184248793:web:f6c3291e963c05468bc3c8",
+    measurementId: "G-KMKKLJDT68"
+  });
