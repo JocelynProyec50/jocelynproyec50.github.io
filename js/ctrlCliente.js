e322bd2671cddeeb636c25ef03f599f662810c60
JocelynProyec50 <85643687+JocelynProyec50@users.noreply.github.com>
2021-06-17 01:49:51 -0500

    Update ctrlCliente.js

---
+++ js/ctrlCliente.js
@@ -0,0 +1,57 @@
+const daoCliente = getFirestore().collection("Cliente");
+const params = new URL (location.href). searchParams;
+const id= params.get("id");
+const forma= document ["forma"];
+
+getAuth().onAuthStateChanged(protege, muestraError);
+async function busca(){
+  try{
+    const doc= await daoCliente.doc(id).get();
+    if (doc.exists){
+      const data= doc.data();
+      forma.nombre.value=data.nombre || "";
+      forma.telefono.value=data.telefono || "";
+      forma.orden.value=data.orden || "";
+      forma.fecha.value=data.fecha || "";
+      forma.addEventListener("submit",guarda);
+      forma.eliminar.addEventListener("click",elimina);
+    } else {
+      throw new Error(" No se encontró. ");
+    }
+  } catch (e){
+    muestraError(e);
+    muestraClientes();
+  }
+}
+async function guarda(evt){
+  try{
+    evt.preventDefault();
+    const formData=
+      new FormData(forma);
+    const nombre = getString(formData,"nombre").trim();
+    const telefono = getString(formData,"telefono").trim();
+    const orden = getString(formData,"orden").trim();
+    const fecha = getString(formData,"fecha").trim();
+    
+    const modelo={
+      nombre,
+      telefono,
+      orden,
+      fecha
+    };
+    await daoAlumno.doc(id).set(modelo);
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
+async function elimina(){
+  try{
+    if (confirm("Confirmar la " + "eliminación")){
+      await daoCliente.doc(id).delete();
+      muestraClientes();
+    }
+  } catch (e) {
+    muestraError(e);
+  }
+}
