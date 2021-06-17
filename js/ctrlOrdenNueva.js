d243a5b149e1547f67b6469b719057c18b91a1ef
JocelynProyec50 <85643687+JocelynProyec50@users.noreply.github.com>
2021-06-17 00:48:31 -0500

    Create ctrlOrdenNueva.js

---
+++ js/ctrlOrdenNueva.js
@@ -0,0 +1,40 @@
+import{
+  getAuth,
+  getFirestore
+}
+import{
+  getString,
+  muestraError
+}
+import{
+  muestaOrdenes
+}
+const daoOrden=
+ getFirestore().
+  collection("Orden");
+const forma=document("forma");
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {Event} evt */
+async funtion guarda (evt){
+  try{
+    evt.preventDefault();
+    const formData=
+      new FormData(forma);
+    const nombre=getString(formData,"nombre").trim();
+    const telefono=getString(formData,"telefono").trim();
+    const fecha=getString(formData,"fecha").trim();
+    
+    const modelo = {
+      nombre,
+      telefono,
+      fecha
+    };
+    await daoOrden.
+    add(modelo);
+    muestraOrdenes ();
+  } catch (e) {
+    muestraError(e);
+  }
+}
