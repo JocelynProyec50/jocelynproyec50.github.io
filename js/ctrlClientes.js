2bc52c9a83bfa8c052ea1c0fe1a600dbcc256f6c
JocelynProyec50 <85643687+JocelynProyec50@users.noreply.github.com>
2021-06-19 20:16:43 -0500

    Update ctrlClientes.js

--- js/ctrlClientes.js
+++ js/ctrlClientes.js
@@ -1,3 +1,11 @@
+import {
+  getAuth,
+  getFirestore
+}
+import {
+  cod,
+  muestraError
+}
 
 const lista= document.querySelector("#lista");
 const daoCliente= getFirestore().collection("Cliente");
