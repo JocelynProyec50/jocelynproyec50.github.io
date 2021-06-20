54c40deedbc70356cf5cfde187fcce2400750715
JocelynProyec50 <85643687+JocelynProyec50@users.noreply.github.com>
2021-06-19 22:35:21 -0500

    Update ctrlClientes.js

--- js/ctrlClientes.js
+++ js/ctrlClientes.js
@@ -1,44 +1,46 @@
 import {
   getAuth,
   getFirestore
-}
+} from "../lib/fabrica.js";
 import {
   cod,
   muestraError
-}
+} from "../lib/util.js";
 
-const lista= document.querySelector("#lista");
-const daoCliente= getFirestore().collection("Cliente");
+const lista = document.querySelector("#lista");
+const daoCliente = getFirestore().collection("CLIENTE");
 
 getAuth().onAuthStateChanged(protege, muestraError);
 
-funtion consulta() {
+function consulta() {
   daoCliente.orderBy("nombre").onSnapshot(htmlLista, errConsulta);
 }
       
-funtion htmlLista(snap) {
+function htmlLista(snap) {
   let html = "";
   if (snap.size > 0) {
     snap.forEach(doc => html += htmlFila(doc));
   } else {
-    html += <li class="vacio"> --- No hay registros. --- </li>;
+    html += /* html */
+      `<li class="vacio"> -- No hay pasatiempos registrados. -- </li>`;
   }
-  lista.innerHTML=html;
+  lista.innerHTML = html;
 }
 
 function htmlFila(doc) {
-const data= doc.data();
-const orden= cod(data.orden);
-const nombre= cod(data.nombre);
-var fsf= cod(data.fecha);
-var fecha= new Date(fsf);
-var espacio= "[     -     ]";
-var dformat= [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join(' / ');
-const parametros= new URLSearchParams();
-parametros.append("id",doc.id);
-return (
-  `<li>
-    <a class="fila" href="cliente.html?${parametros}">
+  
+  const data = doc.data();
+  const orden = cod(data.orden);
+  const nombre = cod(data.nombre);
+  var fsf = cod(data.fecha);
+  var fecha= new Date(fsf);
+  var espacio= "[     -     ]";
+  var dformat= [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join(' / ');
+  const parámetros= new URLSearchParams();
+  parámetros.append("id", doc.id);
+  return (
+    `<li>
+    <a class="fila" href="cliente.html?${parámetros}">
     <strong class="primario">
       ${orden} ${nombre} ${dformat}
     </strong>
